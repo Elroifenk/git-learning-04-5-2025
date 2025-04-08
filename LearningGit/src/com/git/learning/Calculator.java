2436b24b0fc11c6bbb8147260b1207c8fca002e8
Elroi <Elroifenk@gmail.com>
2025-04-07 21:31:40 -0400

    edited add method, added new multiply method

--- LearningGit/src/com/git/learning/Calculator.java
+++ LearningGit/src/com/git/learning/Calculator.java
@@ -4,6 +4,13 @@ public class Calculator {
 	
 
 	public int add(int x, int y) {
-		return x+y;
+		int result = 0;
+		result =x +y;
+		
+		return result;
+	}
+	
+	public int multiply (int x, int y) {
+		return x * y ;
 	}
 }
